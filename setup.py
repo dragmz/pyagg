c55c6ddaf284ba27835da3ded984a721da47b482
Marcin Zawiejski <dragmz@gmail.com>
2025-02-27 01:44:13 +0100

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyagg',
-    version='1.0',
+    version='1.0.1',
     packages=['pyagg'],
     install_requires=['pyopencl', 'numpy', 'py-algorand-sdk', 'pytest'],
     entry_points={'console_scripts': ['pyagg=pyagg:pyagg_cli', 'pyagg-optimize=pyagg:pyagg_optimize_cli']},
